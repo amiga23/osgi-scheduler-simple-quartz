9a8342b3c387083d89a794195524b65994fbf032
amiga23 <t.scheller@email.de>
2019-02-23 09:02:19 +0000

    Cleanup

--- es.scheller.air.config.logging.development/src/es/scheller/air/config/logging/development/LogConfigurator.java
+++ es.scheller.air.config.logging.development/src/es/scheller/air/config/logging/development/LogConfigurator.java
@@ -16,42 +16,30 @@ import es.scheller.air.config.api.ConfigurationData;
 
 @Component( service = LogConfigurator.class, immediate = true)
 public class LogConfigurator {
-  
+
   @Reference
   ConfigurationAdmin cm;
-  
+
+  public static final String PAX_LOGGING_PID = "org.ops4j.pax.logging";
+
   @Activate
   public void activate() {
-    List<ConfigurationAttribute> attributesLogging = new ArrayList<ConfigurationAttribute>();
-//  attributesLogging.add( new ConfigurationAttribute( "org.ops4j.pax.logging.DefaultServiceLog.level", "WARN" ) );
-//  attributesLogging.add( new ConfigurationAttribute( "DefaultServiceLog.level", "WARN" ) );
-    attributesLogging.add( new ConfigurationAttribute( "log4j.rootLogger", "WARN, A1" ) );
-    attributesLogging.add( new ConfigurationAttribute( "log4j.appender.A1", "org.apache.log4j.ConsoleAppender" ) );
-    attributesLogging.add( new ConfigurationAttribute( "log4j.appender.A1.layout", "org.apache.log4j.PatternLayout" ) );
-    attributesLogging.add( new ConfigurationAttribute( "log4j.appender.A1.layout.ConversionPattern", "%d{ISO8601} | %-5p | %-32.32c{1} | %m%n" ) );
-    attributesLogging.add( new ConfigurationAttribute( "log4j.logger.es.scheller", "DEBUG, osgi:*" ) );  
-//    attributesLogging.add( new ConfigurationAttribute( "log4j.additivity.es.scheller","false" ) );
-    ConfigurationData configLogging = new ConfigurationData( "org.ops4j.pax.logging", attributesLogging );
-    applyConfig( configLogging );
-  }
-  
-  public boolean applyConfig( ConfigurationData configurationData ) {
     try {
-      org.osgi.service.cm.Configuration config = cm.getConfiguration( configurationData.getPid(), null );
       Hashtable< String, String > properties = new Hashtable< String, String >();
-      configurationData.getAttributes().forEach( new Consumer< ConfigurationAttribute >() {
 
-        @Override
-        public void accept( ConfigurationAttribute t ) {
-          properties.put( t.getName(), t.getValue() );
-        }
-      } );
+//  properties.put( "org.ops4j.pax.logging.DefaultServiceLog.level", "WARN" );
+//  properties.put( "DefaultServiceLog.level", "WARN" );
+      properties.put( "log4j.rootLogger", "WARN, A1" );
+      properties.put( "log4j.appender.A1", "org.apache.log4j.ConsoleAppender" );
+      properties.put( "log4j.appender.A1.layout", "org.apache.log4j.PatternLayout" );
+      properties.put( "log4j.appender.A1.layout.ConversionPattern", "%d{ISO8601} | %-5p | %-32.32c{1} | %m%n" );
+      properties.put( "log4j.logger.es.scheller", "DEBUG, osgi:*" );
+//    properties.put( "log4j.putitivity.es.scheller","false" );
+      org.osgi.service.cm.Configuration config = cm.getConfiguration( PAX_LOGGING_PID, null );
       config.update( properties );
       config.update();
-      return true;
     } catch( IOException e ) {
       System.err.println( "Can't set logger configuration." );
-      return false;
     }
   }
 }
